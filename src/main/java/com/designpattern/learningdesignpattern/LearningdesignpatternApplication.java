cdb52e39320f70bbc1753b50c34aa0c70d1d6674
rahulr20y <rahulr20y@gmail.com>
2024-01-20 12:05:39 +0530

    initialization

---
+++ src/main/java/com/designpattern/learningdesignpattern/LearningdesignpatternApplication.java
@@ -0,0 +1,13 @@
+package com.designpattern.learningdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningdesignpatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningdesignpatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designpattern/learningdesignpattern/LearningdesignpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designpattern.learningdesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningdesignpatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
