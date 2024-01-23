2b20b0e5abe2b51e842704135c84aad3afd916ed
rahulr20y <rahulr20y@gmail.com>
2024-01-23 23:23:22 +0530

    Singleton creational pattern

---
+++ src/main/java/com/designpattern/learningdesignpattern/creational/SingletonPattern/Main.java
@@ -0,0 +1,84 @@
+package com.designpattern.learningdesignpattern.creational.SingletonPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("It is used when we have to create only on class");
+        /***
+         * 4 ways to achieve this
+         * 1. Eager
+         * 2. Lazy
+         * 3. Synchronized Method
+         * 4. Double Locking
+         */
+
+        DBConnectionEager connectionEager = DBConnectionEager.getInstance();
+        DBConnectionLazy connectionLazy = DBConnectionLazy.getInstance();
+        DBConnectionSynchronized connectionSynchronized = DBConnectionSynchronized.getInstance();
+        DBConnectionDoubleLocking connectionDoubleLocking = DBConnectionDoubleLocking.getInstance();
+    }
+}
+
+// 1. Eager Initialization
+class DBConnectionEager {
+    private static DBConnectionEager connection = new DBConnectionEager();
+
+    private DBConnectionEager() {
+    }
+
+    public static DBConnectionEager getInstance() {
+        return connection;
+    }
+}
+
+// 2. Lazy initialization
+class DBConnectionLazy {
+    private static DBConnectionLazy connection;
+
+    private DBConnectionLazy() {
+
+    }
+
+    public static DBConnectionLazy getInstance() {
+        if (connection == null) {// issue if multiple threads come simultaneously
+            connection = new DBConnectionLazy();
+        }
+        return connection;
+    }
+}
+
+// 3. Synchronized Method
+
+class DBConnectionSynchronized {
+    private static DBConnectionSynchronized connection;
+
+    private DBConnectionSynchronized() {
+
+    }
+
+    synchronized public static DBConnectionSynchronized getInstance() {
+        if (connection == null) {
+            connection = new DBConnectionSynchronized();
+        }
+        return connection;
+    }
+}
+
+// 4. Double Locking
+class DBConnectionDoubleLocking {
+    private static DBConnectionDoubleLocking connection;
+
+    private DBConnectionDoubleLocking() {
+
+    }
+
+    public static DBConnectionDoubleLocking getInstance() {
+        if (connection == null) {
+            synchronized (DBConnectionDoubleLocking.class) {
+                if (connection == null) {
+                    connection = new DBConnectionDoubleLocking();
+                }
+            }
+        }
+        return connection;
+    }
+}
