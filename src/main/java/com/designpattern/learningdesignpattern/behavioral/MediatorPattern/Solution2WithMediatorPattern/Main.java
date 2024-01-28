4b08878e2cbe732a38aafc370e61a2ab8a581100
rahulr20y <rahulr20y@gmail.com>
2024-01-28 11:10:31 +0530

    add change

---
+++ src/main/java/com/designpattern/learningdesignpattern/behavioral/MediatorPattern/Solution2WithMediatorPattern/Main.java
@@ -0,0 +1,7 @@
+package com.designpattern.learningdesignpattern.behavioral.MediatorPattern.Solution2WithMediatorPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
